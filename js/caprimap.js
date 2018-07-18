74095b8c089aaaeec45be5822e92711a1f443f0a
aga98 <aga98@users.noreply.github.com>
2018-07-18 13:16:46 +0200

    dmw

---
+++ js/caprimap.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.flagcont').css({
+     'background':'-webkit-linear-gradient(left,#cc0000,white,#009246)', 
+});
+    initMap();
+});
+
+
+function initMap(){
+	var mymap = L.map('map').setView([43.987021, -77.819589], 9);
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiYWdhOTgiLCJhIjoiY2pqb2pmbXM4MHRwazN3bGU1c2RwYjEwNiJ9.AiTusF62WVe7FiBxkVq0CA'
+	}).addTo(mymap);
+
+	L.marker([44.342769, -76.047971]).addTo(mymap).bindPopup('THOUSAND ISLANDS');
+    L.marker([43.656390, -79.383693]).addTo(mymap).bindPopup('RED LOBSTER');
+    L.marker([43.642523, -79.387054]).addTo(mymap).bindPopup('CN TOWER');
+    L.marker([43.652362, -79.383666]).addTo(mymap).bindPopup('TORONTO CITY HALL');
+}
