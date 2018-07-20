24cf17bf28c6c041d0018c3f7b7916968879d2cd
aga98 <aga98@users.noreply.github.com>
2018-07-20 11:11:01 +0200

    DMW

---
+++ js/geirangermap.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('.flagcont').css({
+     'background':'-webkit-linear-gradient(left,#E50000,white,#04013C)', 
+});
+    initMap();
+});
+
+
+function initMap(){
+	var mymap = L.map('map').setView([62.077856, 7.240526], 12);
+    mymap.scrollWheelZoom.disable();
+    mymap.on('focus', function() { mymap.scrollWheelZoom.enable(); });
+    mymap.on('blur', function() { mymap.scrollWheelZoom.disable(); });
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiYWdhOTgiLCJhIjoiY2pqb2pmbXM4MHRwazN3bGU1c2RwYjEwNiJ9.AiTusF62WVe7FiBxkVq0CA'
+	}).addTo(mymap);
+
+    L.marker([62.102301, 7.199237]).addTo(mymap).bindPopup('GEIRANGERFJORD');
+    L.marker([62.048879, 7.270051]).addTo(mymap).bindPopup('DALSNIBBA');
+    L.marker([62.030576, 7.278597]).addTo(mymap).bindPopup('DJUPVATNET LAKE');
+
+    
+}
